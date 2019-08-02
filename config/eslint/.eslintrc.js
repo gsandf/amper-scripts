533c0ca531bb75f88da241dfd54d30f6f62ff118
Blake Knight <bknight@gsandf.com>
2019-08-02 13:34:22 -0500

    use babel-eslint

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -2,6 +2,7 @@ const { projectPackageJson } = require('../../src/utils');
 
 module.exports = {
   extends: ['gsandf-react', 'prettier'],
+  parser: 'babel-eslint',
   settings: {
     react: {
       version: projectPackageJson.dependencies.react ? 'detect' : 'latest'
